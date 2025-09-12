6a075a4186d0a7f1cabe00e3fdbdc2c6eec2f46c
Marselllniiboyy <marseljayawijaya@gmail.com>
2025-09-12 09:15:15 +0800

    Add toggle dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,65 @@
-// Navbar Fixed
-window.onscroll = () => {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
+// // Navbar Fixed
+// window.onscroll = () => {
+//   const header = document.querySelector("header");
+//   const fixedNav = header.offsetTop;
 
-  if (window.scrollY > fixedNav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
+//   if (window.scrollY > fixedNav) {
+//     header.classList.add("navbar-fixed");
+//   } else {
+//     header.classList.remove("navbar-fixed");
+//   }
+// };
+
+// //hamburger and nav
+// const hamburger = document.querySelector("#hamburger");
+// const navMenu = document.querySelector("#nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("hamburger-active");
+//   navMenu.classList.toggle("hidden");
+// });
+
+$(document).ready(function () {
+  // Navbar Fixed
+  var header = $("header");
+  var fixedNav = header.offset().top;
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > fixedNav) {
+      header.addClass("navbar-fixed");
+    } else {
+      header.removeClass("navbar-fixed");
+    }
+  });
+
+  // Hamburger and nav
+  var hamburger = $("#hamburger");
+  var navMenu = $("#nav-menu");
+
+  hamburger.on("click", function () {
+    $(this).toggleClass("hamburger-active");
+    navMenu.toggleClass("hidden");
+  });
+
+  // Dark mode toggle
+  var html = $("html");
+  var darkToggle = $("#dark-toggle");
+
+  // cek localStorage
+  if (localStorage.theme === "dark") {
+    html.addClass("dark");
   }
-};
 
-//hamburger and nav
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
+  // dark mode toggle
+  darkToggle.on("click", function () {
+    html.toggleClass("dark");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
+    if (html.hasClass("dark")) {
+      localStorage.theme = "dark";
+      console.log("dark");
+    } else {
+      localStorage.theme = "light";
+      console.log("light");
+    }
+  });
 });
